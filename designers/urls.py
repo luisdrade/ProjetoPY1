56f363048176574b98f4abcf2aa7fdb07ac09d2c
Luís Filipe <luisfilipem13@gmail.com>
2025-04-14 21:48:50 -0300

    Correção url designers

--- designers/urls.py
+++ designers/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import DesignerViewSet
 
 router = DefaultRouter()
-router.register(r'designers', DesignerViewSet) #aqui se registra o endpoint de designers e o viewset que será utilizado
+router.register(r'', DesignerViewSet) #aqui se registra o endpoint de designers e o viewset que será utilizado
 
 urlpatterns = [
     path('', include(router.urls)),
