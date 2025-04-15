86e0d02173639e712bf52b67cf568806ba459dce
Luís Filipe <luisfilipem13@gmail.com>
2025-04-14 21:24:57 -0300

    Criando Swagger

--- agencia_aem/settings.py
+++ agencia_aem/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'orcamentos',
     'projeto',
     'servicos',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- agencia_aem/urls.py
+++ agencia_aem/urls.py
@@ -16,8 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path
+
+# Swagger
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title= "API - Agência AEM",
+        default_version='v1',
+        description="Documentação da API da Agência AEM",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="aemagencia@gmail.com"),
+        license=openapi.License(name="MIT License")
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
     path('admin/', admin.site.urls),
     path('api/clientes/', include('cliente.urls')),
     path('api/designers/', include('designers.urls')),
